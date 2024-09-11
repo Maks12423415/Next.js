4f74502e07f3635b2907e8bd5a27a49d6c6bf112
Maks Samborski <makxc7@gmail.com>
2024-09-11 21:57:05 +0200

    Initial commit

---
+++ base-next_js/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className=" border-2 p-10  border-solid border-white rounded-lg">
+        Maks Samborski
+      </div>
+    </div>
+  );
+}
